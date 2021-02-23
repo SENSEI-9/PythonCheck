63a35b551e07ebe2eb4c25ca8984edd0c1e38c5f
SENSEI-9 <78782813+SENSEI-9@users.noreply.github.com>
2021-02-23 11:30:25 +0545

    work2

---
+++ pythonworkshop/python1/2.py
@@ -0,0 +1,5 @@
+#Write a program that reads the length of the base and the height of a right-angled triangle and prints the are. Every number is given on a seperate line,
+base=float(input('enter the base:'))
+height=float(input('enter the height:'))
+area=1/2*base*height
+print('the are of triangle is',area)
