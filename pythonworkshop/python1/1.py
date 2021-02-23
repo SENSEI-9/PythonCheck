f85d5bae39a21b64d5550553c3c5b5d355a7615f
SENSEI-9 <78782813+SENSEI-9@users.noreply.github.com>
2021-02-23 11:18:10 +0545

    work1

---
+++ pythonworkshop/python1/1.py
@@ -0,0 +1,6 @@
+#write a program that takes thress numbers and prints their sum. Every number is given on a seperate line
+number1=int(input('enter a value:'))
+number2=int(input('enter a value:'))
+number3=int(input('enter a value:'))
+sum=number1+number2+number3
+print('the total value is',sum)
