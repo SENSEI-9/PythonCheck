91eb58939babe9c6a120c32e20c954c1e9c1dc62
SENSEI-9 <78782813+SENSEI-9@users.noreply.github.com>
2021-03-02 12:17:09 +0545

    initial commit

--- pythonworkshop/python1/5.py
+++ pythonworkshop/python1/5.py
@@ -1,17 +1,27 @@
-#write a program to find the area of a rectangle,area of a triangle,area of circle
-#area of rectangle
-length=int(input('enter the length'))
-breadth =int(input('enter the breadth'))
-area=length*breadth
-print('The area of rectangle is',area)
+'''5. A school decided to replace the desks in three classrooms. Each desk sits two students.
+Given the number of students in each class, print the smallest possible number of desks that
+can be purchased.The program should read three integers: the number of students in each of the
+ three classes,a,b and c respectively.
+In the first test are three group. The first group has 20 students and thus needs 10 desks. The
+second group has 21 students, so they can get by with no fewer than 11 desks. 11 desks are also
+enough for the third group of 22 students. So, we need 32 desks in total.'''
 
-#area of triangle
-length1=float(input('enter the length'))
-base=float(input('enter the base'))
-area1=1/2*length1*base
-print('the are of triangle is',area1)
-
-#area of circle
-radius=float(input('enter the radius'))
-area3=22/7*radius**2
-print('the area of cirlce is',area3)
+no_students_class1=int(input('enter the number of students in first class: '))
+no_student_class2=int(input('enter the number of students in second class: '))
+no_student_class3=int(input('enter the number of students in third class: '))
+desk_in_class1=(no_students_class1//2)
+remaining_desks1=(no_students_class1%2)
+total_desks_required1=desk_in_class1+remaining_desks1
+print('the total no of desk in first class is',total_desks_required1)
+desk_in_class2=(no_student_class2//2)
+remaining_desks2=(no_student_class2%2)
+total_desks_required2=desk_in_class2+remaining_desks2
+print('the total no of desk in second class is',total_desks_required2)
+desk_in_class3=(no_student_class3//2)
+remaining_desks3=(no_student_class3%2)
+total_desks_required3=(desk_in_class3+remaining_desks3)
+print('the total no of desk required in class 3 is', total_desks_required3)
+total_students=no_students_class1+no_student_class2+no_student_class3
+print('the total no of student is',total_students)
+total_desks=(total_desks_required1+total_desks_required2+total_desks_required3)
+print('the total desks are',total_desks)
