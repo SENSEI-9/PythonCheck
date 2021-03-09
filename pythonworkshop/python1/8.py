bdafbb19044ef0d0e58fa32e05b93f0ecaf3550a
SENSEI-9 <78782813+SENSEI-9@users.noreply.github.com>
2021-03-09 11:20:44 +0545

    BMI of a person

---
+++ pythonworkshop/python1/8.py
@@ -0,0 +1,9 @@
+'''6. Solve each of the following problems using python scripts.
+ Make sure you use appropriate variable names and comments.
+ when there is final answer , have python is to the screen.'''
+mass=float(input('enter the mass of person in kg'))
+height=float(input('enter the height of the person in meter: '))
+BMI= (mass/(height*height))
+print(f'the BMI index of a person is {BMI}')
+
+
